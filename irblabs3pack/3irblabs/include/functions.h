bffb9c1eefddb2af7e14837c8ff7549103b94a38
Костян <varenik.kotyarka@gmail.com>
2025-11-27 23:28:02 +0300

    did first functions

--- irblabs3pack/3irblabs/include/functions.h
+++ irblabs3pack/3irblabs/include/functions.h
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 typedef struct Node {
-    double* data;
+    Liver data;
     struct Node *prev;
     struct Node *next;
 } Node;
@@ -14,3 +14,53 @@ typedef struct {
     size_t size;
 } LinkedList;
 
+LinkedList create_list(void) {
+    LinkedList list;
+    list.head = NULL;
+    list.tail = NULL;
+    list.size = 0;
+    return list;
+}
+
+void erase_list(LinkedList *list) {
+    if (list == NULL) return;
+
+    Node *current = list->head;
+    while (current != NULL) {
+        Node *next = current->next;
+        free(current);
+        current = next;
+    }
+
+    list->head = NULL;
+    list->tail = NULL;
+    list->size = 0;
+}
+
+void delete_list(LinkedList *list) {
+    if (list == NULL) return;
+    erase_list(list);
+    free(list);
+}
+
+void push_back_list(LinkedList *list, Liver value) {
+    if (list == NULL) return;
+
+    Node *new_node = (Node*)malloc(sizeof(Node));
+    if (new_node == NULL) {
+        fprintf(stderr, "Malloc failes");
+        return;
+    }
+
+    new_node->data = value;
+    new_node->next = NULL;
+    new_node->prev = list->tail;
+
+    if (list->tail != NULL){
+        list->tail->next = new_node;
+    } else {
+        list->head=new_node;
+    }
+    list->tail = new_node;
+    list->size++;
+}
